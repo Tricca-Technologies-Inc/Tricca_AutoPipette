685037a8102950350027a93a2f0c5aa38b8c4030
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-01-29 14:29:46 -0700

    Update autopipette.py
    messing with touch

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -1077,7 +1077,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
             touch_depth = loc_dest.get_dip_distance(volume) + 1
             self.gcode_wait(2000) # 1.5 second hold
             self.move_to_z(Coordinate(x=coor_dest.x, y=coor_dest.y, z=touch_depth))
-            self.gcode_wait(1500) # 1.5 second hold
+            self.gcode_wait(3000) # 3 second hold
             self.move_to_z(Coordinate(x=coor_dest.x, y=coor_dest.y, z=loc_dest.get_dip_distance(volume)))
             self.gcode_wait(self.pipette_params.wait_movement)
 
@@ -1300,6 +1300,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if not keep_tip and not self.has_liquid:
 
             self.dispose_tip()
+
 
 
 
