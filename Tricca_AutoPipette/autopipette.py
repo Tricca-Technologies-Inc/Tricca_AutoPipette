7a516eb7748395badbe1f4902d6be7c77c459d88
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-02-02 16:29:14 -0700

    Update autopipette.py
    prewet 3 times...

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -977,7 +977,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if prewet:
             #dip_z = loc_source.get_dip_distance(volume)
             
-            for _ in range(1):
+            for _ in range(3):
                  # Dip into the liquid
                 dip_dist = loc_source.get_dip_distance(volume)
                 self.dip_z_down(coor_source, dip_dist)
@@ -1320,6 +1320,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if not keep_tip and not self.has_liquid:
 
             self.dispose_tip()
+
 
 
 
