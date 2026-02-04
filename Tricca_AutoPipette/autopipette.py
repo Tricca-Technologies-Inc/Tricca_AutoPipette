b5b859bc22dfbcd88ac8e677f10c818ee67a80d0
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-02-04 15:00:43 -0700

    Update autopipette.py

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -1004,7 +1004,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
                     y=coor_source.y,
                     z=raise_z
                 ))
-                self.gcode_wait(self.pipette_params.wait_aspirate+3000)
+                self.gcode_wait(self.pipette_params.wait_aspirate)
                 
                 self.home_pipette_stepper_disp(tot_vol,
                     (self.pipette_params.speed_pipette_down))
@@ -1330,6 +1330,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if not keep_tip and not self.has_liquid:
 
             self.dispose_tip()
+
 
 
 
