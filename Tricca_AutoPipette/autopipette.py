665a0d622b39de797a43ceae0f53a4738e5bc34b
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-02-09 14:28:23 -0700

    Update autopipette.py
    speed up touch wait

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -1105,9 +1105,9 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         # 3) Optional touch (a small single dip)
         if touch:
             touch_depth = loc_dest.get_dip_distance(volume) + 1
-            self.gcode_wait(2000) # 1.5 second hold
+            self.gcode_wait(1500) # 1.5 second hold
             self.move_to_z(Coordinate(x=coor_dest.x, y=coor_dest.y, z=touch_depth))
-            self.gcode_wait(3000) # 3 second hold
+            self.gcode_wait(2000) # 2 second hold
             self.move_to_z(Coordinate(x=coor_dest.x, y=coor_dest.y, z=loc_dest.get_dip_distance(volume)))
             self.gcode_wait(self.pipette_params.wait_movement)
 
@@ -1330,6 +1330,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if not keep_tip and not self.has_liquid:
 
             self.dispose_tip()
+
 
 
 
