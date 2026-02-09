59108be01381ed7c1692a837a8473b90786cc828
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-02-09 12:40:42 -0700

    Update autopipette.py
    remove extra air for prewet

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -979,12 +979,12 @@ class AutoPipette(metaclass=AutoPipetteMeta):
             
             for _ in range(3):
                 #grab extra air
-                AIR_CUSHION_UL = self.pipette_params.ext_air
-                self.plunge_down(
-                    AIR_CUSHION_UL,
-                    self.pipette_params.speed_pipette_down
-                )
-                self.gcode_wait(self.pipette_params.wait_aspirate)
+                #AIR_CUSHION_UL = self.pipette_params.ext_air
+                #self.plunge_down(
+                    #AIR_CUSHION_UL,
+                    #self.pipette_params.speed_pipette_down
+                #)
+                #self.gcode_wait(self.pipette_params.wait_aspirate)
 
                  # Dip into the liquid
                 dip_dist = loc_source.get_dip_distance(volume)
@@ -1330,6 +1330,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if not keep_tip and not self.has_liquid:
 
             self.dispose_tip()
+
 
 
 
