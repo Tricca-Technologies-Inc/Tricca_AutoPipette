4c3c767e90b0cb02a8578ced608d59974986b4c6
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-02-17 13:09:28 -0700

    Update autopipette.py

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -1025,7 +1025,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
                     #z=raise_z
                 #))
                 #self.move_to(coor_source)
-            AIR_CUSHION_UL = ext_air
+            AIR_CUSHION_UL = ext_vol
             self.plunge_down(
                 AIR_CUSHION_UL,
                 self.pipette_params.speed_pipette_down
@@ -1333,6 +1333,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if not keep_tip and not self.has_liquid:
 
             self.dispose_tip()
+
 
 
 
