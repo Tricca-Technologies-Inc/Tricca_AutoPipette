97f0df8d260fed02670825b9a375d168c27892ba
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-02-23 13:37:35 -0700

    Update autopipette.py
    changing dispense acceleration from 800 to 1000

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -562,7 +562,8 @@ class AutoPipette(metaclass=AutoPipetteMeta):
 
         self._buffer_command(
             f"MANUAL_STEPPER STEPPER={stepper} SPEED={speed} "
-            f"MOVE={stepsq} STOP_ON_ENDSTOP=1 SET_POSITION=0 ACCEL=800\n"
+            # accel was 800
+            f"MOVE={stepsq} STOP_ON_ENDSTOP=1 SET_POSITION=0 ACCEL=1000\n"
             f"MANUAL_STEPPER STEPPER={stepper} SET_POSITION=0\n")
 
     def get_gcode(self) -> list[str]:
@@ -1333,6 +1334,7 @@ class AutoPipette(metaclass=AutoPipetteMeta):
         if not keep_tip and not self.has_liquid:
 
             self.dispose_tip()
+
 
 
 
