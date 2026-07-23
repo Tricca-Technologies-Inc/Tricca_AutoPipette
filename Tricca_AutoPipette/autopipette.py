dc8d6d88ccea82cbf1fa8b321424da61c6b7916c
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-07-23 15:29:01 -0600

    Update autopipette.py

--- Tricca_AutoPipette/autopipette.py
+++ Tricca_AutoPipette/autopipette.py
@@ -1043,7 +1043,7 @@ class AutoPipette:
             if (self.pipette_params.ext_air+volume+aft_vol >= self.pipette_params.max_vol):
                 ext_vol = self.pipette_params.max_vol - (volume+aft_vol+2)
                 if ext_vol < 0:
-                ext_vol = 0
+                    ext_vol = 0
             else:
                 ext_vol = self.pipette_params.ext_air
         else:
