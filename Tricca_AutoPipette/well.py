f68b1bc7277182d120768859d256beaeb6006e56
samackay89 <67442369+samackay89@users.noreply.github.com>
2026-08-07 12:42:45 -0600

    Implement FStube method and update Well class
    Added FStube method for calculating liquid dip distance in a 0.5mL screw cap tube. Updated Well class to include FStube strategy and handle dip volume.

--- Tricca_AutoPipette/well.py
+++ Tricca_AutoPipette/well.py
@@ -34,6 +34,29 @@ class DipStrategies:
             well.dip_curr = well.dip_btm
         return well.dip_curr
 
+    @staticmethod
+    def FStube(well: Well, volume: float) -> float:
+        """Return the dip distance of a liquid in the cone section of a 0.5mL screw cap tube."""
+        # Make sure dip_btm is defined and return just the top if not
+        if well.dip_btm is None or well.dip_vol is None:
+            raise ValueError(
+                "FStube strategy requires dip_btm and dip_vol")
+        # Calculate the current height of liquid in the tube
+        height_current = 2.61+0.0836*well.dip_vol+1e-4*(well.dip_vol**2)-1.83e-6*(well.dip_vol**3)
+        # Calculate and update the new volume in the tube
+        well.dip_vol = well.dip_vol - volume
+        if well.dip_vol < 0:
+            well.dip_vol = 0
+        # Calculate the new height of the liquid in the vial
+        height_new = 2.61+0.0836*well.dip_vol+1e-4*(well.dip_vol**2)-1.83e-6*(well.dip_vol**3)
+        # Find the change in height of the liquid
+        height_change = height_current - height_new # all already in mm
+        well.dip_curr += height_change
+        # Make sure to never dip further than dip_btm
+        if well.dip_curr > well.dip_btm:
+            well.dip_curr = well.dip_btm
+        return well.dip_curr, well.dip_vol
+
 
 class WellParams(BaseModel):
     """A data validation class to hold Well related variables."""
@@ -41,6 +64,7 @@ class WellParams(BaseModel):
     coor: Coordinate
     dip_top: confloat(gt=0)
     dip_btm: Optional[confloat(gt=0)] = None
+    dip_vol: Optional[confloat(gt=0)] = None
     dip_func: Optional[Callable[[Well, float], float]] = \
         DipStrategies.simple
     well_diameter: Optional[confloat(gt=0)] = None
@@ -63,20 +87,23 @@ class WellParams(BaseModel):
 class Well:
     """A vessel to hold chemicals."""
 
-    STRATEGIES: list[str] = ["simple", "cylinder"]
+    STRATEGIES: list[str] = ["simple", "cylinder","FStube"]
     NAME_TO_STRAT: dict[str, Callable[[Well, float], float]] = {
         "simple": DipStrategies.simple,
         "cylinder": DipStrategies.cylinder,
+        "FStube": DipStrategies.FStube,
     }
     STRAT_TO_NAME: dict[Callable[[Well, float], float], str] = {
         DipStrategies.simple: "simple",
         DipStrategies.cylinder: "cylinder",
+        DipStrategies.FStube: "FStube",
     }
 
     def __init__(self,
                  coor: Coordinate,
                  dip_top: float,
                  dip_btm: Optional[float] = None,
+                 dip_vol: Optional[float] = None,
                  dip_func:
                      Optional[Callable[[Well, float], float]] = None,
                  diameter: Optional[float] = None):
@@ -84,6 +111,8 @@ class Well:
         self.coor = coor
         self.dip_top = dip_top
         self.dip_btm = dip_btm
+        #initialize the starting volume
+        self.dip_vol = dip_vol
         # Always start at the top of the well
         self.dip_curr = dip_top
         self.dip_func = dip_func
